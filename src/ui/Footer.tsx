e93e565cc309134381a3de383df8d42f96451313
Nishanth <nishanth1596@gmail.com>
2025-03-28 10:57:15 +0530

    feat: - Add form validation to the input element in the footer - Add error message

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,7 +1,30 @@
+import { useForm } from "react-hook-form";
 import Logo from "./Logo";
 import NavBar from "./NavBar";
+import { validateEmail } from "../utils/utils";
+
+type FormValues = {
+  email: string;
+};
 
 function Footer() {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  function onSubmit(data: FormValues) {
+    if (!validateEmail(data.email)) {
+      setError("email", {
+        message: "Whoops, make sure it's an email",
+      });
+    }
+
+    return;
+  }
+
   return (
     <footer className="mt-[123px] text-center text-white">
       <section className="bg-Blue px-8 py-[3.75rem]">
@@ -12,15 +35,24 @@ function Footer() {
           </span>
         </h3>
 
-        <form className="mt-8">
+        <form className="mt-8" onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="email" className="sr-only">
             Email address
           </label>
           <input
-            className="text-Black w-full rounded-[5px] bg-white px-5 py-2.5 text-sm leading-7 font-normal tracking-[0.25px]"
+            className={`text-Black ${errors.email ? "outline-Orange focus:outline-Orange active:outline-Orange outline-2" : ""} active:outline-Blue focus:outline-Blue w-full rounded-[5px] bg-white px-5 py-2.5 text-sm leading-7 font-normal tracking-[0.25px]`}
             type="text"
             placeholder="Enter your email address"
+            {...register("email", {
+              required: "Email is required",
+            })}
           />
+          {errors.email && (
+            <p className="bg-Orange outline-Orange mt-0 rounded-b-[5px] px-3 pt-1 pb-1.5 text-left text-[10px] font-medium text-white italic opacity-100 outline-2">
+              {errors.email.message}
+            </p>
+          )}
+
           <button className="bg-Orange mt-4 block w-full rounded-[5px] py-2.5 text-sm leading-7 font-medium tracking-[0.25px]">
             Contact us
           </button>
